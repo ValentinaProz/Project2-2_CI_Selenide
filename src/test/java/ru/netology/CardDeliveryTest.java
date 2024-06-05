8d61891b73d997af488e3660086473a27fdd2046
Valentina.Pro <verano86@mail.ru>
2024-06-05 22:28:54 +0300

    Appveyor changed

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
@@ -14,12 +15,13 @@ public class CardDeliveryTest {
     public String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
     @Test
     public void shouldBeCorrectlyFilledOut() {
         open("http://localhost:9999");
         String planningDate = generateDate(5);
         $("[data-test-id='city'] input").setValue("Краснодар");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord (Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Игорь Чижиков");
         $("[data-test-id='phone'] input").setValue("+79990000000");
